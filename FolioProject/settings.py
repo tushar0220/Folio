f81190fed0f96b192128540394bc4641d365e810
tushar0220 <87055239+tushar0220@users.noreply.github.com>
2021-10-10 16:58:30 +0530

    second commit

--- FolioProject/settings.py
+++ FolioProject/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-94hjwp2g((rx1hdbhmso(_h5e577c0a8kb)2o@u%r0u%f+kwvd'
+#SECRET_KEY = 'django-insecure-94hjwp2g((rx1hdbhmso(_h5e577c0a8kb)2o@u%r0u%f+kwvd'
 
-#import os
-#SECRET_KEY = os.environ['SECRET_KEY']
+import os
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
